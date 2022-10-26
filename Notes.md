24b8ddf860ec4323398c32ed8077c30eebb7a993
andytgarcia <andytgarcia27@gmail.com>
2022-10-26 10:48:53 -0700

    put rectangles on screen

---
+++ Notes.md
@@ -0,0 +1,36 @@
+#Programming Notes
+
+#Principles of Programming
+
+Ideas and concepts to help you succeed in programming
+
+- ***Always keep program in a working state AT ALL TIMES***
+- Attempt to code small additions. Don't implement too much detail or functionality
+- Each idea/addition should be implemented in its own function definition
+- Name functions and variables meaningfully
+- If something breaks...***STOP***. Seek help or begin to debug
+- It is okay to not know how to do something! Look it up
+  - [Pygame Help](https://www.pygame.org/docs/ref/draw.html)
+  - [Python Help](https://www.w3scho;s.com/python/)
+
+##Create Task Requirements
+
+- **A purpose** - why did you write this application
+- **A List** - a list and how it reduced complexity or how you could not write the app without one
+- **A Function**
+  - *Sequence* - code that runs over several lined over
+  - *Selection* - if statements
+  - *Iteration* - looping
+  - *Parameter*- input to the function
+    - This input must change the result of the function
+
+### Parameter
+
+What is a parameter?
+
+```python
+def function(x, y):
+
+```
+
+x and y in the above function definition are parameters
--- main.py
+++ main.py
@@ -1,15 +1,67 @@
 import pygame
 
+player = {"x": 640,
+          "y": 360,
+          "size": 10,
+          "speed": .5,
+          "color": pygame.Color(255, 0, 255)}
+
+
+map = []
+
+
+def drawMap():
+    for currentRect in map:
+        #draw a rectangle
+        pygame.draw.rect(screen, pygame.Color(0, 255, 255), currentRect)
+
+def createmap1():
+    map.append(pygame.Rect(400, 200, 100, 250))
+    map.append(pygame.Rect(300, 100, 10, 500))
+    map.append(pygame.Rect(0, 0, 1280, 10))
+    map.append(pygame.Rect(0, 0, 10, 720))
+    map.append(pygame.Rect(900, 100, 300, 10))
+
+
+def clearScreen():
+
+    pygame.draw.rect(screen, pygame.Color(0, 0, 0), (0,0, 1280, 720))
+
+def drawPlayer():
+    pygame.draw.circle(screen, player["color"], (player["x"], player["y"]), player["size"])
+
+
+
+def playerMovement():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        player["x"] += player["speed"]
+    if keys[pygame.K_s]:
+        player["y"] += player["speed"]
+    if keys[pygame.K_a]:
+        player["x"] -= player["speed"]
+    if keys[pygame.K_w]:
+        player["y"] -= player["speed"]
+
+
 # start of program
 pygame.init()  # start engine
 FPS = 60  # 60 frames per second
 fpsClock = pygame.time.Clock
 screen = pygame.display.set_mode((1280, 720))
 gameOver = False
+createmap1()
 
 while not gameOver:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameOver = True
+
+    playerMovement()
+
+    #draw code
+    clearScreen()
+    drawMap()
+    drawPlayer()
+
     pygame.display.flip()
- 
