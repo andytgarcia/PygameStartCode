2afc314fce8e9ddf59b10d89190aa5c10d94a672
andytgarcia <andytgarcia27@gmail.com>
2022-11-03 10:47:40 -0700

    First Commit

--- Notes.md
+++ Notes.md
@@ -34,3 +34,39 @@ def function(x, y):
 ```
 
 x and y in the above function definition are parameters
+
+### For Looping through a List
+
+Assume the following list
+
+```python
+bullets = []
+
+bullets.append((100, 100, 5))
+bullets.append((400, 300, 5))
+bullets.append((340, 560, 5))
+```
+
+How do we loop through this bullet list?
+
+```python
+for currentBullet in bullet:
+    #code to process each bullet goes here
+    #each bullet will be stored in variable currentBullet
+```
+
+In general. it looks like
+
+```python
+for temporaryVariable in listName:
+    #code to process each item as stores in temporary variable
+```
+
+Things we may for loop through a list for:
+ - draw all things in a list
+ - check everything for collisions
+ - choose something from the list based upon some criteria
+   - choose a hard question for quiz game
+   - take a look at all player one's bullets
+ - see if something is true about the list
+   - in Wordle, check the word against every letter to see if its correct
--- main.py
+++ main.py
@@ -1,14 +1,50 @@
+import random
+
 import pygame
 
-player = {"x": 640,
+import time
+
+player1 = {"x": 640,
           "y": 360,
+          "oldx": 640,
+          "oldy": 360,
           "size": 10,
           "speed": .5,
-          "color": pygame.Color(255, 0, 255)}
+          "color": pygame.Color(255, 0, 0),
+           "lastDir": "right",
+           "shotTimeStamp": 0,
+           "nextShot": 0,
+           "score": 0}
 
+
+player2 =  {"x": 840,
+          "y": 160,
+          "oldx": 840,
+          "oldy": 160,
+          "size": 10,
+          "speed": .5,
+          "color": pygame.Color(0, 0, 255),
+            "lastDir": "right",
+            "shotTimeStamp": 0,
+            "nextShot": 0,
+            "score": 0}
 
 map = []
 
+bullets = [] #list of bullets in the game
+"""
+bullets[0] = x
+bullets[1] = y
+bullets[2] = size
+bullets[3] = player
+bullets[4] = color
+bullets[5] = x velocity
+bullets[6] = y velocity
+"""
+
+
+def getPlayerCollisionRectangle(player):
+    return pygame.Rect(player["x"] - player["size"], player["y"] - player["size"], player["size"] * 2, player["size"] * 2)
 
 def drawMap():
     for currentRect in map:
@@ -27,27 +63,204 @@ def clearScreen():
 
     pygame.draw.rect(screen, pygame.Color(0, 0, 0), (0,0, 1280, 720))
 
-def drawPlayer():
-    pygame.draw.circle(screen, player["color"], (player["x"], player["y"]), player["size"])
+def drawPlayers():
+    pygame.draw.circle(screen, player1["color"], (player1["x"], player1["y"]), player1["size"])
+    pygame.draw.circle(screen, player2["color"], (player2["x"], player2["y"]), player2["size"])
 
+
+def drawBullets():
+    for b in bullets:
+        pygame.draw.circle(screen, b["color"], (b["x"], b["y"]), b["size"])
 
 
 def playerMovement():
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        player["x"] += player["speed"]
-    if keys[pygame.K_s]:
-        player["y"] += player["speed"]
-    if keys[pygame.K_a]:
-        player["x"] -= player["speed"]
-    if keys[pygame.K_w]:
-        player["y"] -= player["speed"]
+    player1["oldx"] = player1["x"]
+    player1["oldy"] = player1["y"]
 
+    player2["oldx"] = player2["x"]
+    player2["oldy"] = player2["y"]
+
+    if keys[pygame.K_d]:
+        player1["x"] += player1["speed"]
+        player1["lastDir"] = "right"
+    if keys[pygame.K_s]:
+        player1["y"] += player1["speed"]
+        player1["lastDir"] = "down"
+    if keys[pygame.K_a]:
+        player1["x"] -= player1["speed"]
+        player1["lastDir"] = "left"
+    if keys[pygame.K_w]:
+        player1["y"] -= player1["speed"]
+        player1["lastDir"] = "up"
+    if keys[pygame.K_f] and player1["nextShot"] < time.time_ns():
+        if player1["lastDir"] == "right":
+            bullets.append({"x": player1["x"],
+                            "y": player1["y"],
+                            "size": 5,
+                            "owner": "p1",
+                            "color": player1["color"],
+                            "xvel": 20,
+                            "yvel": 0})
+            player1["nextShot"] = time.time_ns() + 100000000
+
+        elif player1["lastDir"] == "left":
+            bullets.append({"x": player1["x"],
+                                "y": player1["y"],
+                                "size": 5,
+                                "owner": "p1",
+                                "color": player1["color"],
+                                "xvel": -20,
+                                "yvel": 0})
+            player1["nextShot"] = time.time_ns() + 100000000
+        elif player1["lastDir"] == "up":
+            bullets.append({"x": player1["x"],
+                            "y": player1["y"],
+                            "size": 5,
+                            "owner": "p1",
+                            "color": player1["color"],
+                            "xvel": 0,
+                            "yvel": -20})
+            player1["nextShot"] = time.time_ns() + 100000000
+
+        elif player1["lastDir"] == "down":
+            bullets.append({"x": player1["x"],
+                            "y": player1["y"],
+                            "size": 5,
+                            "owner": "p1",
+                            "color": player1["color"],
+                            "xvel": 0,
+                            "yvel": 20})
+            player1["nextShot"] = time.time_ns() + 100000000
+
+
+
+
+
+    #p2
+    if keys[pygame.K_RIGHT]:
+        player2["x"] += player2["speed"]
+        player2["lastDir"] = "right"
+    if keys[pygame.K_DOWN]:
+        player2["y"] += player2["speed"]
+        player2["lastDir"] = "down"
+    if keys[pygame.K_LEFT]:
+        player2["x"] -= player2["speed"]
+        player2["lastDir"] = "left"
+    if keys[pygame.K_UP]:
+        player2["y"] -= player2["speed"]
+        player2["lastDir"] = "up"
+    if keys[pygame.K_m] and player2["nextShot"] < time.time_ns():
+        if player2["lastDir"] == "right":
+            bullets.append({"x": player2["x"],
+                            "y": player2["y"],
+                            "size": 5,
+                            "owner": "p2",
+                            "color": player2["color"],
+                            "xvel": 20,
+                            "yvel": 0})
+            player2["nextShot"] = time.time_ns() + 100000000
+
+        elif player2["lastDir"] == "left":
+            bullets.append({"x": player2["x"],
+                                "y": player2["y"],
+                                "size": 5,
+                                "owner": "p2",
+                                "color": player2["color"],
+                                "xvel": -20,
+                                "yvel": 0})
+            player2["nextShot"] = time.time_ns() + 100000000
+        elif player2["lastDir"] == "up":
+            bullets.append({"x": player2["x"],
+                            "y": player2["y"],
+                            "size": 5,
+                            "owner": "p2",
+                            "color": player2["color"],
+                            "xvel": 0,
+                            "yvel": -20})
+            player2["nextShot"] = time.time_ns() + 100000000
+
+        elif player2["lastDir"] == "down":
+            bullets.append({"x": player2["x"],
+                            "y": player2["y"],
+                            "size": 5,
+                            "owner": "p2",
+                            "color": player2["color"],
+                            "xvel": 0,
+                            "yvel": 20})
+            player2["nextShot"] = time.time_ns() + 100000000
+
+
+
+def updateBullets():
+    for b in bullets:
+        b["x"] += b["xvel"] #x += x velocity
+        b["y"] += b["yvel"] #y += y velocity
+        if ifOffScreen(b["x"], b["y"]):
+            bullets.remove(b)
+
+        player1Rectangle = getPlayerCollisionRectangle(player1)
+        player2Rectangle = getPlayerCollisionRectangle(player2)
+        bulletRect = pygame.Rect(b["x"] - b["size"], b["y"] - b["size"], b["size"] * 2, b["size"] * 2)
+        if pygame.Rect.colliderect(bulletRect, player1Rectangle) and b["owner"] == "p2":
+            player2["score"] += 100
+            newRound()
+        if pygame.Rect.colliderect(bulletRect, player2Rectangle) and b["owner"] == "p1":
+            player1["score"] += 100
+            newRound()
+
+        #check this bullet for collision in every wall
+        for wall in map:
+            bulletRect = pygame.Rect(b["x"] - b["size"], b["y"] - b["size"], b["size"] * 2, b["size"] * 2)
+            if pygame.Rect.colliderect(wall, bulletRect):
+                b["xvel"] *= -1
+                b["yvel"] *= -1
+
+
+def ifOffScreen(x, y):
+    if x <0 or x > 1280 or y < 0 or y > 720:
+        return True
+    else:
+        return False
+
+def checkPlayerCollision(player):
+    playRect = getPlayerCollisionRectangle(player)
+    for wall in map:
+        if pygame.Rect.colliderect(playRect, wall):
+            player["x"] = player["oldx"]
+            player["y"] = player["oldy"]
+            print("Collision")
+            #player["color"] = (random.randint(0, 255),
+             #                   random.randint(0, 255),
+              #                  random.randint(0, 255))
+
+
+
+
+def drawPlayerCollisionBox(player):
+    pygame.draw.rect(screen, pygame.Color(255, 0, 255), getPlayerCollisionRectangle(player), 1)
+
+
+def newRound():
+    bullets.clear()
+    player1["x"] = 100
+    player1["y"] = 100
+    player2["x"] = 900
+    player2["y"] = 600
+
+
+def drawHUD():
+    textSurface = myfont.render("Player 1 Score: " + str(player1["score"]), True, (255, 255, 255))
+    screen.blit(textSurface, (50, 30))
+    textSurface = myfont.render("Player 2 Score: " + str(player2["score"]), True, (255, 255, 255))
+    screen.blit(textSurface, (1050, 30))
 
 # start of program
 pygame.init()  # start engine
+pygame.font.init()
+myfont = pygame.font.SysFont('Times New Roman', 23)
 FPS = 60  # 60 frames per second
-fpsClock = pygame.time.Clock
+fpsClock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 720))
 gameOver = False
 createmap1()
@@ -59,9 +272,20 @@ while not gameOver:
 
     playerMovement()
 
+
+    updateBullets()
+    checkPlayerCollision(player1)
+    checkPlayerCollision(player2)
+
+
     #draw code
     clearScreen()
     drawMap()
-    drawPlayer()
+    drawBullets()
+    drawPlayers()
+    drawPlayerCollisionBox(player1)
+    drawPlayerCollisionBox(player2)
+    drawHUD()
+
 
     pygame.display.flip()
